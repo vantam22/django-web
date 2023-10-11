b78c29b91ec23f6617ae40ee2aaf3ba4ad2d12c7
vantam22 <147504624+vantam22@users.noreply.github.com>
2023-10-11 08:55:20 +0700

    lập trình web python django

---
+++ azwafitness/authapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from authapp import views
+urlpatterns = [
+    path('',views.Home,name="Home"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="proflie"),
+    path('gallery',views.gallery,name="gallery"),
+    path('attendance',views.attendance,name="attendance"),
+]
